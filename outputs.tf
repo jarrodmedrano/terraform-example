5d92adc5a9ec90865091e05debfeff66a4b5e27d
Jarrod Medrano <jarrod.medrano@changehealthcare.com>
2023-06-21 12:02:47 -0500

    add splat

--- outputs.tf
+++ outputs.tf
@@ -21,3 +21,9 @@ output "Datetime" {
   value       = local.time
   sensitive   = false
 }
+
+output "private_addresses" {
+  description = "The private ip address of the server"
+  value       = aws_instance.server[*].private_ip
+  sensitive   = false
+}
