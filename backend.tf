0079eb3c73c609f7872cca1db5e2fc258ec2cbb3
Jarrod Medrano <jarrod.medrano@changehealthcare.com>
2023-06-21 20:34:24 -0500

    added backend conf

---
+++ backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket = "work-tf-remote-state"
+    key    = "s3_backend.tfstate"
+    region = "us-east-2"
+    # variables not allowed here. use backend.conf
+    # access_key = var.TF_VAR_AWS_ACCESS_KEY_ID
+    # secret_key = var.TF_VAR_AWS_SECRET_ACCESS_KEY
+  }
+}
